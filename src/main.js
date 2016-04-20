d9857ce9d489c69975a6130350210765b7d535a4
victmo <victmo@gmail.com>
2016-04-20 17:49:25 -0400

    Change project name, fixed jquery dependency issue and added packing to build script

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
 'use strict';
 
-const path = require('path');
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
-// Report crashes to our server.
-electron.crashReporter.start();
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
-
-// PATCH
-// Fix wrong node_module paths
-module.paths.push(path.resolve('node_modules'));
-module.paths.push(path.resolve('../node_modules'));
-module.paths.push(path.resolve(__dirname, '..', '..', '..', '..', 'resources', 'app', 'node_modules'));
-module.paths.push(path.resolve(__dirname, '..', '..', '..', '..', 'resources', 'app.asar', 'node_modules'));
-
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -27,6 +15,7 @@ app.on('window-all-closed', () => {
 	//if (process.platform != 'darwin')
 	app.quit();
 });
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -45,7 +34,7 @@ app.on('ready', () => {
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
 
 	// Open the DevTools.
-	mainWindow.webContents.openDevTools();
+	//mainWindow.webContents.openDevTools();
 
 	// Emitted when the window is closed.
 	mainWindow.on('closed', function() {
